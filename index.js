90ca437f430259daf5aa38bf7882ca0066ace05e
tuyishimejeanpierre-byte <tuyishime.jeanpierre@student.moringaschool.com>
2026-04-06 13:44:58 +0300

    read index.js

--- index.js
+++ index.js
@@ -45,3 +45,29 @@ const bookStore = {
 
 // Write your code here!
 
+// 1. Update header
+const bookStoreTitle = document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
+
+// 2. Create book elements
+const bookList = document.getElementById("book-list");
+
+bookStore.books.forEach((book) => {
+  const bookContainer = document.createElement("li");
+  const bookTitle = document.createElement("h3");
+  const bookAuthor = document.createElement("p");
+  const bookImage = document.createElement("img");
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookImage.src = book.imageUrl;
+
+  bookContainer.append(bookTitle, bookAuthor,bookImage );
+  bookList.appendChild(bookContainer);
+});
+
+// 3. Bonus: delete element
+const elementToDelete = document.getElementById("delete-this");
+if (elementToDelete) {
+  elementToDelete.remove();
+}
